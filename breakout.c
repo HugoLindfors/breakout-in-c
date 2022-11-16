071aecacc229c7f27732aca86b964cffa32c15a7
Neutron-Cntrl <hugo.lindfors2@elev.ga.ntig.se>
2022-11-16 14:05:33 +0100

    commit

--- breakout.c
+++ breakout.c
@@ -25,21 +25,22 @@ typedef struct Ball
 typedef struct Box
 {
     Vector2 position;
-    float radius;
+    float width;
+    float height;
     bool active;
     Color color;
 } Box;
 
 // global variables declaration
-static const int screen_width = 800;
-static const int screen_height = 450;
-static bool game_over = false;
-static bool game_is_paused = false;
-static bool victory = false;
+const int screen_width = 800;
+const int screen_height = 450;
+bool game_over = false;
+bool game_is_paused = false;
+bool victory = false;
 
-static Paddle paddle = { 0 };
-static Ball ball = { 0 };
-static Box box = { 0 };
+Paddle paddle = {0};
+Ball ball = {0};
+Box box[60] = {0};
 
 // program main entry point
 int main(void)
@@ -48,33 +49,99 @@ int main(void)
 
     SetTargetFPS(60); // set our game to run at 60 frames-per-second
 
+    paddle.position = (Vector2){screen_width / 2, screen_height / 2 + 200};
+    paddle.velocity = (Vector2){10.0f, 0.0f};
+    paddle.acceleration = 0;
+    paddle.width = 250;
+    paddle.height = 10;
+    paddle.color = YELLOW;
+
+    ball.position = (Vector2){screen_width / 2, screen_height / 2};
+    ball.velocity = (Vector2){0.0f, 5.0f};
+    ball.radius = 10.0f;
+    ball.color = LIGHTGRAY;
+
+    // box.position = (Vector2){screen_width / 2, screen_height / 2 - 200};
+    // box.width = 200;
+    // box.height = 20;
+    // box.active = true;
+    // box.color = YELLOW;
+
+    for (int i = 0; i < 60; i++)
+    {
+        box[i].position = (Vector2){screen_width / 2, screen_height / 2 - 200};
+        box[i].width = 200;
+        box[i].height = 20;
+        box[i].active = true;
+        box[i].color = YELLOW;
+    }
+    
+
+
+
     // main game loop
     while (!WindowShouldClose()) // detect window close or esc key
     {
+
+        Rectangle paddle_rec = {
+            .x = paddle.position.x,
+            .y = paddle.position.y,
+            .width = paddle.width,
+            .height = paddle.height};
+
+        // Rectangle box_rec = {
+        //     .x = box.position.x,
+        //     .y = box.position.y,
+        //     .width = box.width,
+        //     .height = box.height};
+
+        Rectangle box_rec[60];
+
+        for (int i = 0; i < 60; i++)
+        {
+            box_rec[i] = {
+                .x = box[i].position.x,
+                .y = box[i].position.y,
+                .width = box[i].width,
+                .height = box[i].height;
+            };
+        };
+
         // update
-        int pos_x, pos_y;
-        int vel_x, vel_y,
-        victory = false;
-        game_is_paused = false;
 
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
+            paddle.position.x += paddle.velocity.x;
 
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
+            paddle.position.x -= paddle.velocity.x;
 
-        paddle.position = (Vector2){screen_width / 2, screen_height / 2};
-        paddle.velocity = (Vector2){10.0f, 0.0f};
-        paddle.acceleration = 0;
-        paddle.width = 250;
-        paddle.height = 10;
-        paddle.color = YELLOW;
+        if (CheckCollisionCircleRec(ball.position, ball.radius, paddle_rec))
+        {
+            ball.velocity.x = -ball.velocity.x;
+            ball.velocity.y = -ball.velocity.y;
+        }
 
-        if (IsKeyDown(KEY_RIGHT)) paddle.position.x += paddle.velocity.x;
-        if (IsKeyDown(KEY_LEFT)) paddle.position.x -= paddle.velocity.x;
+        // if (CheckCollisionCircleRec(ball.position, ball.radius, box_rec) && box.active)
+        // {
+        //     ball.velocity.x = -ball.velocity.x;
+        //     ball.velocity.y = -ball.velocity.y;
+        //     box.active = false;
+        // }
+
+        ball.position.x += ball.velocity.x;
+        ball.position.y += ball.velocity.y;
 
         // draw
         BeginDrawing();
 
-            ClearBackground(BLACK);
-            
-            DrawRectangle(paddle.position.x, paddle.position.y, paddle.width, paddle.height, paddle.color);
+        ClearBackground(BLACK);
+
+        // if (box.active)
+        //     DrawRectangle(box.position.x, box.position.y, box.width, box.height, box.color);
+
+        DrawRectangle(paddle.position.x, paddle.position.y, paddle.width, paddle.height, paddle.color);
+
+        DrawCircle(ball.position.x, ball.position.y, ball.radius, ball.color);
 
         EndDrawing();
     };
